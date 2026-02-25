26e25fcb0208cb94e3f30822dec37b3f2fd2fa6e
Ji Admin <admin@ji-clipboard.com>
2026-02-25 15:56:38 +0800

    fix: 将 view.html 移动到 public 目录确保被部署

--- Pages/src/version.ts
+++ Pages/src/version.ts
@@ -1,3 +1,3 @@
 // 自动生成的版本号文件
-export const APP_VERSION = '1.0.5'
-export const BUILD_TIME = '2026-02-25T07:47:49.339Z'
+export const APP_VERSION = '1.0.6'
+export const BUILD_TIME = '2026-02-25T07:56:17.526Z'
