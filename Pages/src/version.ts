4eb8a6c56734bd9404cdd3bd5f813566650c688e
Ji Admin <admin@ji-clipboard.com>
2026-02-25 16:18:46 +0800

    chore: 重新构建前端

--- Pages/src/version.ts
+++ Pages/src/version.ts
@@ -1,3 +1,3 @@
 // 自动生成的版本号文件
-export const APP_VERSION = '1.0.7'
-export const BUILD_TIME = '2026-02-25T07:59:29.810Z'
+export const APP_VERSION = '1.0.8'
+export const BUILD_TIME = '2026-02-25T08:18:35.390Z'
