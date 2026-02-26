8a5f68ed1ac962141e7fcee77f146092d83e8ed1
Ji Admin <admin@ji-clipboard.com>
2026-02-26 11:11:21 +0800

    refactor: 移除长按菜单中的复制选项，复制只能通过点击图标

--- Pages/src/components/ClipboardPage.vue
+++ Pages/src/components/ClipboardPage.vue
@@ -722,11 +722,6 @@ function handleTouchEnd(event: TouchEvent, item: any, index: number) {
           <mdui-icon slot="icon" name="open_in_new"></mdui-icon>
           在新标签页打开
         </mdui-menu-item>
-        <!-- 复制选项（所有情况都显示） -->
-        <mdui-menu-item @click="copyClipboard(contextMenuItem?.content); closeContextMenu()">
-          <mdui-icon slot="icon" name="content_copy"></mdui-icon>
-          复制
-        </mdui-menu-item>
         <mdui-menu-item @click="deleteClipboard(contextMenuItem?.index, true); closeContextMenu()">
           <mdui-icon slot="icon" name="delete" style="color: var(--mdui-color-error)"></mdui-icon>
           <span style="color: var(--mdui-color-error)">删除</span>
