30958e787ddb4a4ac80492d682f0547157789d64
Ji Admin <admin@ji-clipboard.com>
2026-03-01 16:39:00 +0800

    feat: 在顶部导航栏右上角添加版本号显示

--- Pages/src/version.ts
+++ Pages/src/version.ts
@@ -1,3 +1,3 @@
 // 自动生成的版本号文件
-export const APP_VERSION = '1.1.45'
-export const BUILD_TIME = '2026-03-01T08:37:04.783Z'
+export const APP_VERSION = '1.1.46'
+export const BUILD_TIME = '2026-03-01T08:38:51.993Z'
