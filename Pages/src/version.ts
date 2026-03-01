e6d95d6bdc12d2faa5dd4d8bb1aaabe15c083350
Ji Admin <admin@ji-clipboard.com>
2026-03-02 02:18:23 +0800

    feat: add custom favicon for clipboard app

--- Pages/src/version.ts
+++ Pages/src/version.ts
@@ -1,4 +1,4 @@
 // 自动生成的版本号文件
-export const APP_VERSION = '1.1.85'
-export const BUILD_TIME = '2026-03-01T17:27:03.075Z'
+export const APP_VERSION = '1.1.86'
+export const BUILD_TIME = '2026-03-01T18:18:16.808Z'
 export const BUILD_DATE = '20260301'
