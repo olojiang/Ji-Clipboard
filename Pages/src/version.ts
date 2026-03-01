d2108b672aed14ef912f8983dd9d635d502d8b3b
Ji Admin <admin@ji-clipboard.com>
2026-03-01 18:57:56 +0800

    chore: bump version to 1.1.59

--- Pages/src/version.ts
+++ Pages/src/version.ts
@@ -1,4 +1,4 @@
 // 自动生成的版本号文件
-export const APP_VERSION = '1.1.58'
-export const BUILD_TIME = '2026-03-01T10:19:42.495Z'
+export const APP_VERSION = '1.1.59'
+export const BUILD_TIME = '2026-03-01T10:57:36.965Z'
 export const BUILD_DATE = '20260301'
